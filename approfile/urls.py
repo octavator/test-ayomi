0d89d8c7a461eb1d968bb3a8b04aba558ae7dbfc
t-decagny <theophile.de-cagny@suricats-consulting.com>
2020-10-02 17:31:11 +0200

    took out base slug for the app webprofile

--- approfile/urls.py
+++ approfile/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('webprofile/', include('webprofile.urls')),
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('', include('webprofile.urls')),
 ]
